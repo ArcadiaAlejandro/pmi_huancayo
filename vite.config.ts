217f4a9adafc0c3a7152d17ae1d5d020532311d0
Alejandro Antonio <alejandrodelacruzcharun@gmail.com>
2024-07-30 00:16:09 -0500

    Se ha modificado el nombre del repositorio porque no hacia conexion a las rutas

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://ArcadiaAlejandro.github.io/PMI_CHAPTER_HUANCAYO"
+  base: "https://ArcadiaAlejandro.github.io/pmi_huancayo"
 })
